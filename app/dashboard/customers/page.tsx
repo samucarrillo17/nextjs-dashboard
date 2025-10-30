88a623f3ff884179d5a4569c0af53a6e6b55d7cd
samucarrillo17 <samucarrillo016@gmail.com>
2025-10-30 16:45:28 -0500

    nextjs-dashboard

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function Customers() {
+  return (
+    <div>
+      <h1>Customers Page</h1>
+      <p>Welcome to the Customers page!</p>
+    </div>
+  );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function Invoices() {
+    return (
+        <div>
+            <h1>Invoices Page</h1>
+            <p>Welcome to the Invoices page!</p>
+        </div>
+    );
+}
